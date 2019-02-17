ff3f5b53456586b0c78b2d65b23fd1939ce92e55
Hugo Oliveira <35900662+HROlive@users.noreply.github.com>
2019-02-17 18:22:22 +0000

    Create Lab 1 - Practice with ML APIs in Cloud Datalab.sql

---
+++ Applying Machine Learning to your Data with GCP/Lab 1 - Practice with ML APIs in Cloud Datalab.sql
@@ -0,0 +1,13 @@
+# Setting up Google Cloud Datalab on a Compute Engine VM
+
+# Enable APIs and Get API key
+
+-- APIs and services - Library - Enable  necessary APIs - APIs and services - Credentials -
+-- Create credentials - API Key
+
+# Invoke ML APIs from Datalab
+
+# Create a new Datalab instance and open the ipynb 
+# https://storage.googleapis.com/data-insights-course/mlapis.ipynb
+
+-- Open Cloud Shell - datalab create apidemo
